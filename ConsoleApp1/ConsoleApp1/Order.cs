2045f7a5895dade12e8dc59da1d6668f199e2b8e
adeliafarida <nafiatuadelia33718@gmail.com>
2025-05-16 11:04:56 +0700

    Perbaiki konflik

---
+++ ConsoleApp1/ConsoleApp1/Order.cs
@@ -0,0 +1,8 @@
+﻿public class Order
+{
+    public string? Id { get; set; }
+    public string? Barang{ get; set; }
+    public string? Status { get; set; }
+    public string? Harga { get; set; }
+   
+}
