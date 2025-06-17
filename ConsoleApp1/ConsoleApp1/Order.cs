0408c62bc7cb788a84d7b69009f4955c55f5da58
DeoFaradyS <deofarady43@gmail.com>
2025-06-17 13:03:12 +0700

    ⚙️ fix: memperbaiki konflik dari merge branch adelmanajemenpesanan

--- ConsoleApp1/ConsoleApp1/Order.cs
+++
@@ -1,8 +0,0 @@
-﻿public class Order
-{
-    public string? Id { get; set; }
-    public string? Barang{ get; set; }
-    public string? Status { get; set; }
-    public string? Harga { get; set; }
-   
-}
