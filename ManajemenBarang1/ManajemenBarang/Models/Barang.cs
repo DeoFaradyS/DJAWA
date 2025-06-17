5527e011672789b1e3c5b62f05b3e2623bbf34bc
DeoFaradyS <deofarady43@gmail.com>
2025-06-17 13:23:58 +0700

    ⚙️ fix: mengatur fiolder merge dari branch asti-manajemen-barang

--- ManajemenBarang1/ManajemenBarang/Models/Barang.cs
+++
@@ -1,7 +0,0 @@
-﻿public class Barang
-{
-    public string Nama { get; set; }
-    public string Deskripsi { get; set; }
-    public int Jumlah { get; set; }
-    public decimal Harga { get; set; }
-}
